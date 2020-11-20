9a48028a17c087a68aeed7b3f053f8cc0f573770
Jacek Kuśnierz <jaku@grapeup.com>
2020-11-20 21:04:07 +0100

    reformat

--- lambda.py
+++ lambda.py
@@ -1,10 +1,9 @@
 import json
-import boto3
-import botocore
 import os
-import io
-import urllib3
 import zipfile
+
+import boto3
+
 
 def lambda_handler(event, context):
     print("started")
@@ -12,19 +11,20 @@ def lambda_handler(event, context):
     print("downloading obj")
     s3.download_file(event['bucket_name'], 'aws_root.zip', '/tmp/aws_root.zip')
     print("streamed obj")
-    os.system('ls -la /tmp/aws_root.zip')
+
     with zipfile.ZipFile('/tmp/aws_root.zip', 'r') as zip_ref:
         print("starting extracting")
         zip_ref.extractall('/mnt/cern_root')
+
     print("finished extracting obj")
-        
-    result=os.system('''
-    export PATH=/mnt/cern_root/chroot/usr/local/sbin:/mnt/cern_root/chroot/usr/local/bin:/mnt/cern_root/chroot/usr/sbin:/mnt/cern_root/chroot/usr/bin:/mnt/cern_root/chroot/sbin:/mnt/cern_root/chroot/bin:$PATH && \
-    export LD_LIBRARY_PATH=/mnt/cern_root/chroot/usr/lib64:/mnt/cern_root/chroot/usr/lib:/usr/lib64:/usr/lib:$LD_LIBRARY_PATH && \
-    export roothome=/mnt/cern_root/root_install && \
-    chmod 777 /mnt/cern_root/chroot/usr/bin/python3.7 && \
-    chmod 777 /mnt/cern_root/root_install/bin/root-config && \
-    . ${roothome}/bin/thisroot.sh && /mnt/cern_root/chroot/usr/bin/python3.7 ${roothome}/PyRDF/introduction.py
+
+    result = os.system('''
+        export PATH=/mnt/cern_root/chroot/usr/local/sbin:/mnt/cern_root/chroot/usr/local/bin:/mnt/cern_root/chroot/usr/sbin:/mnt/cern_root/chroot/usr/bin:/mnt/cern_root/chroot/sbin:/mnt/cern_root/chroot/bin:$PATH && \
+        export LD_LIBRARY_PATH=/mnt/cern_root/chroot/usr/lib64:/mnt/cern_root/chroot/usr/lib:/usr/lib64:/usr/lib:$LD_LIBRARY_PATH && \
+        export roothome=/mnt/cern_root/root_install && \
+        chmod 777 /mnt/cern_root/chroot/usr/bin/python3.7 && \
+        chmod 777 /mnt/cern_root/root_install/bin/root-config && \
+        . ${roothome}/bin/thisroot.sh && /mnt/cern_root/chroot/usr/bin/python3.7 ${roothome}/PyRDF/introduction.py
     ''')
 
     return {
