2980c995aa2f631d19e340b060a7bf777837f208
Jacek Kuśnierz <jaku@grapeup.com>
2020-12-14 08:58:15 +0100

    remove not needed functionality

--- lambda.py
+++ lambda.py
@@ -20,23 +20,23 @@ def lambda_handler(event, context):
 
     print("finished extracting obj")
 
-    result = os.system('''
-        export PATH=/mnt/cern_root/chroot/usr/local/sbin:/mnt/cern_root/chroot/usr/local/bin:/mnt/cern_root/chroot/usr/sbin:/mnt/cern_root/chroot/usr/bin:/mnt/cern_root/chroot/sbin:/mnt/cern_root/chroot/bin:$PATH && \
-        export LD_LIBRARY_PATH=/mnt/cern_root/chroot/usr/lib64:/mnt/cern_root/chroot/usr/lib:/usr/lib64:/usr/lib:$LD_LIBRARY_PATH && \
-        export CPATH=/mnt/cern_root/chroot/usr/include:$CPATH && \
-        export roothome=/mnt/cern_root/root_install && \
-        chmod 777 /mnt/cern_root/chroot/usr/bin/python3.7 && \
-        chmod 777 /mnt/cern_root/root_install/bin/root-config && \
-        cd /tmp && . ${roothome}/bin/thisroot.sh && \
-        /mnt/cern_root/chroot/usr/bin/python3.7 ${roothome}/PyRDF/introduction.py
-    ''')
+    # result = os.system('''
+    #     export PATH=/mnt/cern_root/chroot/usr/local/sbin:/mnt/cern_root/chroot/usr/local/bin:/mnt/cern_root/chroot/usr/sbin:/mnt/cern_root/chroot/usr/bin:/mnt/cern_root/chroot/sbin:/mnt/cern_root/chroot/bin:$PATH && \
+    #     export LD_LIBRARY_PATH=/mnt/cern_root/chroot/usr/lib64:/mnt/cern_root/chroot/usr/lib:/usr/lib64:/usr/lib:$LD_LIBRARY_PATH && \
+    #     export CPATH=/mnt/cern_root/chroot/usr/include:$CPATH && \
+    #     export roothome=/mnt/cern_root/root_install && \
+    #     chmod 777 /mnt/cern_root/chroot/usr/bin/python3.7 && \
+    #     chmod 777 /mnt/cern_root/root_install/bin/root-config && \
+    #     cd /tmp && . ${roothome}/bin/thisroot.sh && \
+    #     /mnt/cern_root/chroot/usr/bin/python3.7 ${roothome}/PyRDF/xrootd.py
+    # ''')
 
-    if os.WEXITSTATUS(result) != 0:
-        return {
-            'statusCode': 500,
-            'body': json.dumps('Failed to extract ROOT to EFS!'),
-            'result': json.dumps(os.WEXITSTATUS(result))
-        }
+    # if os.WEXITSTATUS(result) != 0:
+    #     return {
+    #         'statusCode': 500,
+    #         'body': json.dumps('Failed to extract ROOT to EFS!'),
+    #         'result': json.dumps(os.WEXITSTATUS(result))
+    #     }
 
     return {
         'statusCode': 200,
