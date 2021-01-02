d033e9ff73e93b48078994d07f8ae32176f50b8d
Jacek Kuśnierz <jaku@grapeup.com>
2021-01-02 12:26:09 +0100

    more fun

--- lambda.py
+++ lambda.py
@@ -10,15 +10,19 @@ bucket = os.environ.get('bucket')
 def lambda_handler(event, context):
     print("started")
     s3 = boto3.client('s3')
-    print("downloading obj")
-    s3.download_file(bucket, 'aws_root.zip', '/tmp/aws_root.zip')
-    print("streamed obj")
+    if not os.path.isdir('/mnt/cern_root/chroot'):
+        print("ROOT not present")
+        print("Downloading ROOT")
+        s3.download_file(bucket, 'aws_root.zip', '/tmp/aws_root.zip')
+        print("Streamed ROOT")
 
-    with zipfile.ZipFile('/tmp/aws_root.zip', 'r') as zip_ref:
-        print("starting extracting")
-        zip_ref.extractall('/mnt/cern_root')
+        with zipfile.ZipFile('/tmp/aws_root.zip', 'r') as zip_ref:
+            print("Starting extracting to EFS")
+            zip_ref.extractall('/mnt/cern_root')
 
-    print("finished extracting obj")
+        print("Finished extracting ROOT")
+    else:
+        print("ROOT already extracted")
 
     result = os.system('''
         export PATH=/mnt/cern_root/chroot/usr/local/sbin:/mnt/cern_root/chroot/usr/local/bin:/mnt/cern_root/chroot/usr/sbin:/mnt/cern_root/chroot/usr/bin:/mnt/cern_root/chroot/sbin:/mnt/cern_root/chroot/bin:$PATH && \
@@ -27,9 +31,11 @@ def lambda_handler(event, context):
         export roothome=/mnt/cern_root/root_install && \
         chmod 777 /mnt/cern_root/chroot/usr/bin/python3.7 && \
         chmod 777 /mnt/cern_root/root_install/bin/root-config && \
-        cd /tmp && . ${roothome}/bin/thisroot.sh && \
-        /mnt/cern_root/chroot/usr/bin/python3.7 ${roothome}/PyRDF/xrootd.py
+        cd /tmp && . ${roothome}/bin/thisroot.sh  && \
+        python3.7 -m pip install cloudpickle
     ''')
+    # move inside os.system if wanting to test ASAP for internet connection and working ROOT
+    # /mnt/cern_root/chroot/usr/bin/python3.7 ${roothome}/PyRDF/xrootd.py
 
     if os.WEXITSTATUS(result) != 0:
         return {
